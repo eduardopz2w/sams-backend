ad224d335535149698b6bc58d02efc6778630200
wilmer2 <mvc_wil@outlook.es>
2016-02-11 22:44:31 -0430

    details and audit corrected

--- app/Sams/Manager/OccurrenceEditManager.php
+++ app/Sams/Manager/OccurrenceEditManager.php
@@ -4,9 +4,12 @@ namespace Sams\Manager;
 
 class OccurrenceEditManager extends BaseManager {
   public function getRules() {
+    $date = current_date();
+    $day = 1;
+    $date =  add_date($day, $date);
     $rules = [
       'case_situation' => 'required',
-      'date' => 'required|date'
+      'date' => 'required|date|before:'.$date
     ];
 
     return $rules;
@@ -16,7 +19,8 @@ class OccurrenceEditManager extends BaseManager {
     $messages = [
       'case_situation.required' => 'Ingrese situacion del caso',
       'date.required'  => 'Ingrese fecha',
-      'date.date' => 'Ingrese formato valido para fecha'
+      'date.date' => 'Ingrese formato valido para fecha',
+      'date.before' => 'Fecha no ha pasado'
     ];
 
     return $messages;
