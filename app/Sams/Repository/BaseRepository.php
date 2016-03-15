9b79a49838a4c2d4e4724d2e55cef0d6e2686134
wilmer2 <mvc_wil@outlook.es>
2016-03-15 13:14:27 -0430

    BaseRepo brackert

--- app/Sams/Repository/BaseRepository.php
+++ app/Sams/Repository/BaseRepository.php
@@ -2,22 +2,20 @@
 
 namespace Sams\Repository;
 
-abstract class BaseRepository{
+abstract class BaseRepository {
 	
 	protected $model;
 
-	public function __construct()
-
-	{
-			$this->model = $this->getModel();
+	public function __construct() {
+		$this->model = $this->getModel();
 	}
 
 	abstract public function getModel();
 
-	public function find($id)
-
-	{
-			return $this->model->find($id);
+	public function find($id) {
+		return $this->model->find($id);
 	}
+			
+	
 
 }
